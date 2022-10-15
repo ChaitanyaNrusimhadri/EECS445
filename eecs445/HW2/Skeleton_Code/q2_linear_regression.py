a19634b40ab61818f8f60e990c5e4865f54ba552
Chaitanya Nrusimhadri <chaitanyaswami25@gmail.com>
2022-10-15 19:09:03 -0400

    Add files via upload

--- eecs445/HW2/Skeleton_Code/q2_linear_regression.py
+++ eecs445/HW2/Skeleton_Code/q2_linear_regression.py
@@ -13,10 +13,8 @@ def calculate_empirical_risk(X, y, theta):
     # TODO: Implement this function
     sum = 0
     size = np.size(y)
-    #np.apply_along_axis(np.dot(), )
-    for i in size: 
-        sum += (y[i] - (np.dot(theta, X[i,:]))) ** 2 #[i,:] returns entire row
-        sum /= 2
+    for i in range(size): 
+        sum += ((y[i] - (np.dot(theta, X[i,:]))) ** 2)/2 #[i,:] returns entire row
     risk = sum/size
     return risk
 
@@ -35,16 +33,16 @@ def generate_polynomial_features(X, M):
         Phi: np.array, shape (n, M+1)
     """
     # TODO: Implement this function
-    n = len(X)
+    n = X.shape[0]
     Phi = np.zeros((n, M+1))
-    X_arr = []
-    for i in X:
+    #X_arr = []
+    for i in range(X.shape[0]):
         for j in range(M+1):
-            X_arr.append(X^j)
-    Phi.append(X_arr)
+            Phi[i][j] = X[i]**j
+    #Phi.append(X_arr)
     return Phi
 
-def calculate_RMS_Error(X, y, theta):
+def calculate_RMS_Error(X, y, theta, M = 0):
     """
     Args:
         X: np.array, shape (n, d) 
@@ -59,10 +57,10 @@ def calculate_RMS_Error(X, y, theta):
     E_rms = 0
     E_theta = 0
     sum = 0
-    PhiX = generate_polynomial_features(X, M = 2)
+    PhiX = generate_polynomial_features(X, M)
     for i in range(len(X)):
-        sum = np.dot(theta, PhiX)
-        sum = (E_theta - y[i]) ** 2
+        sum = np.dot(theta, PhiX[i])
+        sum = (sum - y[i]) ** 2
         E_theta += sum
     E_rms = mth.sqrt(E_theta/len(X))
     return E_rms
@@ -88,10 +86,15 @@ def ls_gradient_descent(X, y, learning_rate=0):
     new_loss = 0
     #step_size = [1e-4, 1e-3, 1e-2, 1e-1]
     num_iterations = 0
-    while (num_iterations <= 1e6) and (abs(new_loss - prev_loss) <= 1e-10):
-        theta = theta - learning_rate*calculate_empirical_risk(X, y, theta)
+    while (num_iterations <= 1e6) and (abs(new_loss - prev_loss) >= 1e-10):
+        grad_sum = 0
+        for i in range(X.shape[0]):
+            grad_sum += (y[i] - np.dot(theta, X[i])) * (-X[i])
+        theta = theta - learning_rate * grad_sum/X.shape[0]
+        prev_loss = new_loss
         new_loss = calculate_empirical_risk(X, y, theta)
         num_iterations += 1
+    print("iterations = ", num_iterations)
     return theta
 
 
@@ -119,11 +122,14 @@ def ls_stochastic_gradient_descent(X, y, learning_rate=0):
 
     i = 0 #used for accessing current i in X and y matrices during SGD
 
-    while (num_iterations <= 1e6) and (abs(new_loss - prev_loss) <= 1e-10):
-        curr_emp = (y[i] - theta * X[i, :]) ** 2 #empirical risk
-        curr_emp /= 2
-        theta = theta - learning_rate * curr_emp
-        i += 1
+    while (num_iterations <= 1e6) and (abs(new_loss - prev_loss) >= 1e-10):
+        for i in range(X.shape[0]):
+            curr_emp = (y[i] - np.dot(theta, X[i])) * (-X[i])  #empirical risk
+            theta = theta - learning_rate * curr_emp
+            num_iterations += 1
+        prev_loss = new_loss
+        new_loss = calculate_empirical_risk(X,y, theta)
+    print("num_it =", num_iterations)
     return theta
 
 
@@ -142,6 +148,8 @@ def closed_form_optimization(X, y, reg_param=0):
     # TODO: Implement this function
     d = len(X[0])
     theta = np.zeros((d,))
+
+    """
     #(X transposed x X)^-1
     X_transpose = np.transpose(X)
     Xsquare = np.matmul(X_transpose, X) 
@@ -152,6 +160,27 @@ def closed_form_optimization(X, y, reg_param=0):
 
     #Theta optimal
     theta = np.matmul(XInvX, Xy)
+    """
+
+    #Create identity matrix size d x d and multiply reg_param(lambda) to it 
+    #X^T times X is a d x d matrix since X is a n x d matrix
+    id_matrix = np.identity(d)
+    lambda_matrix = reg_param * id_matrix
+
+    #Multiplies X^T to X
+    X_transpose = np.transpose(X)
+    Xsquare = np.matmul(X_transpose, X) 
+
+    #X transposed times y
+    Xy = np.matmul(X_transpose, y)
+
+    #Creates added matrix and inverses it 
+    X_add_lambda = lambda_matrix + Xsquare
+    InvX = np.linalg.inv(X_add_lambda)
+
+    #Multiplies inversed matrix with X^T*y
+    theta = np.matmul(InvX, Xy)
+
     return theta
 
 
@@ -162,16 +191,33 @@ def part_2_1(fname_train):
     X_train, y_train = load_data(fname_train)
     step_size = [1e-4, 1e-3, 1e-2, 1e-1]
 
-    PhiX = generate_polynomial_features(X_train, M = 2)
-    for i in step_size: #Step through different step sizes
-        print("step_size =", i )
-        GD_theta = ls_gradient_descent(X_train, y_train, learning_rate = i)
-        GD_theta = np.dot(GD_theta, PhiX) #Multiplies GD theta with phi(x)
-        print("Gradient Descent =", GD_theta)
+    import timeit
 
-        SGD_theta = ls_stochastic_gradient_descent(X_train, y_train, learning_rate = i)
-        SGD_theta = np.dot(SGD_theta, PhiX) #Multiplies SGD theta with phi(x)
+    PhiX = generate_polynomial_features(X_train, M = 1)
+    """
+    for i in step_size: #Step through different step sizes
+        start = timeit.default_timer()
+        print("step_size =", i )
+        GD_theta = ls_gradient_descent(PhiX, y_train, learning_rate = i)
+        print("Gradient Descent =", GD_theta)
+        stop = timeit.default_timer()
+        print('GD Time: ', stop - start)  
+
+
+        print("----------------")
+        start = timeit.default_timer()
+        SGD_theta = ls_stochastic_gradient_descent(PhiX, y_train, learning_rate = i)
+        #SGD_theta = np.dot(SGD_theta, PhiX) #Multiplies SGD theta with phi(x)
         print("Stochastic Gradient Descent =", SGD_theta)
+        stop = timeit.default_timer()
+        print('SGD Time: ', stop - start) 
+        print("-----------------------------------------------------")
+    """
+    start = timeit.default_timer()
+    closed_form = closed_form_optimization(PhiX, y_train)
+    print("closed form optimization =", closed_form)
+    stop = timeit.default_timer()
+    print('Closed Form Optimization Time: ', stop - start)
 
     print("Done!")
     plt.plot(X_train, y_train, 'ro')
@@ -193,6 +239,15 @@ def part_2_2(fname_train, fname_validation):
     errors_validation = np.zeros((11,))
     # Add your code here
 
+    
+    for i in range(11):
+        PhiX = generate_polynomial_features(X_train, M = i)
+        theta = closed_form_optimization(PhiX, y_train)
+
+        errors_train[i] = calculate_RMS_Error(X_train, y_train, theta, M = i)
+        errors_validation[i] = calculate_RMS_Error(X_validation, y_validation, theta, M = i)
+    
+
     plt.plot(errors_train,'-or',label='Train')
     plt.plot(errors_validation,'-ob', label='Validation')
     plt.xlabel('M')
@@ -210,6 +265,15 @@ def part_2_2(fname_train, fname_validation):
     errors_validation = np.zeros((10,))
     L = np.append([0], 10.0 ** np.arange(-8, 1))
     # Add your code here
+
+    PhiX = generate_polynomial_features(X_train, M = 10)
+
+    lambda_arr = [0, 1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1]
+
+    for i in lambda_arr:
+        theta = closed_form_optimization(PhiX, y_train, reg_param = i)
+        errors_train[i] = calculate_RMS_Error(X_train, y_train, theta, M = i)
+        errors_validation[i] = calculate_RMS_Error(X_validation, y_validation, theta, M = i)
 
     plt.figure()
     plt.plot(L, errors_train, '-or', label='Train')
